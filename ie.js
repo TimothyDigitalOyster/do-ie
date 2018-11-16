0ba36c2609d4377be79b023b6c407a882c7db8bf
Timothy Digital Oyster <timothy@digitaloyster.uk>
2018-11-16 13:50:14 +0000

    Removed all debug for pull

--- ie.js
+++ ie.js
@@ -6,31 +6,21 @@ var standardise = function(id) {
   else if ($("#container_"+id+" label.error").attr("style") == "display: inline;") $("#container_"+id+" label.error").attr("style", "display:inline-block;");
 }
 var observer = new MutationObserver(function(mutations) {
-  console.info("Change");
-
   mutations.forEach(function(mutation) {
-    console.log(mutation);
     if (mutation.type === 'childList' ) {
       var list_values = [].slice.call(list.children).map( function(node) { return node.innerHTML; });
     }
     if (typeof list_values != 'undefined') {
       list_values.forEach(function(value) {
-
         var forFind = /for=\"(.*?)\"/g.exec(value);
-        console.log(forFind[1]);
-        console.info(value);
         if ("#group_"+forFind[1].length > 0) {
-          console.log("group" + "#group_"+forFind[1].length);
           $("#container_"+forFind[1]+" label.error").remove();
           $('#group_'+forFind[1]).after(value);
         } else {
-          console.log("else");
           $("#container_"+forFind[1]+" label.error").remove();
   			  $('#'+forFind[1]).after(value);
         }
-
     		standardise(forFind[1]);
-        //console.log("mUpdates:"+forFind[1]);
         updates.observe(document.querySelector(".content [for='"+forFind[1]+"']"), {
   		      attributes: true,
   		  		childList: false,
@@ -43,15 +33,12 @@ var observer = new MutationObserver(function(mutations) {
 
 var updates = new MutationObserver(function(mutations) {
   $("form label.error").remove();
-  console.info("Updates");
-  console.log(mutations);
   mutations.forEach(function(mutation) {
     var error_id, error_html;
     //console.log(mutation);
     if (mutation.target.control == '' || mutation.target.control == null) {
       error_id = mutation.target.htmlFor;
       error_html = mutation.target.outerHTML;
-      console.log('id:'+error_id+" errorHtml:"+error_html);
       $("#container_"+error_id+" label.error").remove();
       $('#group_'+error_id).after(error_html);
  	    standardise(error_id);
@@ -71,4 +58,4 @@ observer.observe(list, {
   characterData: true
 });
 
-$('<style>.lp-pom-form-field label.error {display: inline-block;position: relative;top: 52px;width: 100%;text-align: left;color:red;}.lp-pom-form-field input.error, .lp-pom-form-field select.error {border:2px solid red !important;}.lp-form-errors {display: block !important;}</style>').appendTo($('head'));
+$('<style>.lp-pom-form-field label.error {display: inline-block;position: relative;top: 52px;width: 100%;text-align: left;color:red;}.lp-pom-form-field input.error, .lp-pom-form-field select.error {border:2px solid red !important;}.lp-form-errors {display: none !important;}</style>').appendTo($('head'));
