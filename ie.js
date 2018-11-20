edcfc916aa19b11ecbc017bd3db5db363460b00b
Timothy Digital Oyster <timothy@digitaloyster.uk>
2018-11-20 14:35:20 +0000

    debug block

--- ie.js
+++ ie.js
@@ -58,4 +58,4 @@ observer.observe(list, {
   characterData: true
 });
 
-$('<style>.lp-pom-form-field label.error {display: inline-block;position: relative;top: 52px;width: 100%;text-align: left;color:red;}.lp-pom-form-field input.error, .lp-pom-form-field select.error {border:2px solid red !important;}.lp-form-errors {display: none !important;}</style>').appendTo($('head'));
+$('<style>.lp-pom-form-field label.error {display: inline-block;position: relative;top: 52px;width: 100%;text-align: left;color:red;}.lp-pom-form-field input.error, .lp-pom-form-field select.error {border:2px solid red !important;}.lp-form-errors {display: block !important;}</style>').appendTo($('head'));
